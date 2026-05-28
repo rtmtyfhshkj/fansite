ae1627a8e49be1a953d48d2af6871941cfc4d052
rtmtyfhshkj <s2srkyyg@gmail.com>
2026-05-28 22:21:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // ===================================================
 //  Supabase の設定（あなたの値に書き換えてください）
 // ===================================================
-const SUPABASE_URL = 'ここにSupabase URLを貼り付け';
-const SUPABASE_ANON_KEY = 'ここにAnon Keyを貼り付け';
+const SUPABASE_URL = 'https://ayikadffpussqepzsmqe.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_K8RL4x6oTXuFvq6j8wOtow_vioSoQID';
 
 // ===================================================
 //  合言葉（好きな言葉に変更してください）
 // ===================================================
-const SECRET_WORD = 'ここに合言葉';
+const SECRET_WORD = 'いさめいと';
